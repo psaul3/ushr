e1cd6dccd6afabbae474ccdbbdf880516dc9826f
Saul Perez <psaul3@gmail.com>
2015-09-06 16:13:04 -0400

    .005
    Debugging touch events

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -11,7 +11,7 @@
     // angular.module is a global place for creating, registering and retrieving Angular modules
     // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
     // the 2nd parameter is an array of 'requires'
-    var app = angular.module('app', ['ionic', 'ngAria' ,'ngMaterial']);
+    var app = angular.module('app', ['ionic', 'ngAria', 'ngMaterial']);
 
     app.filter('range', function () {
         var i;
